99e5e660a1b42e42f9b55c96b9fff10eb6d58984
Hmue Thiha Naung <hmuethihanaung@emial.com>
2021-12-07 16:16:02 +0630

    async data fetching in composition

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-for="post in post" :key="post.id">
+      <h2> {{post.title}} </h2>
+      <p> {{post.body}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['post']
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="home">
-
-  </div>
+  <h2> {{error}} </h2>
+  <SinglePost :post="post"></SinglePost>
 </template>
 
 <script>
+import SinglePost from '../components/SinglePost'
+import { ref } from '@vue/reactivity'
 
 
 export default {
-
+  components: { SinglePost },
+  setup(){
+    let post = ref([]);
+    let error = ref('')
+    let load = async()=> {
+      try{
+        let response =await fetch("http://localhost:3000/posts");
+        if(response.status === 404){
+          throw new Error("url not found")
+        }
+        let datas = await response.json();
+        post.value = datas;
+        console.log(post)
+      }catch(err){
+         error.value=err.message;
+      }
+    }
+    load();
+    return{post,error}
+  }
 }
 </script>
+
+<style scoped>
+  h2{
+    color: crimson;
+  }
+</style>
